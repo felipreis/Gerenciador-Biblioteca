6c51fdb489cbfc921bae0d9ee88f4c63486cfbd0
Felipe Reis <felipereisdaniel@hotmail.com>
2024-04-23 20:27:58 -0300

    inserindo métodos alterar e excluir usuários

--- src/main/java/com/javaswing/view/ConsultarUsuario.java
+++ src/main/java/com/javaswing/view/ConsultarUsuario.java
@@ -42,7 +42,7 @@ public class ConsultarUsuario extends javax.swing.JFrame {
         btnVoltar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("CONSULTA DE LIVROS");
+        setTitle("CONSULTA DE USUARIO");
         setSize(new java.awt.Dimension(900, 500));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosed(java.awt.event.WindowEvent evt) {
