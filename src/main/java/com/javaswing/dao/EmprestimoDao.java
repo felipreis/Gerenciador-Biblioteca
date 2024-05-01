6b6e2dde5a62fb1104ce99bb198da048c5e00429
Felipe Reis <felipreisdaniel@gmail.com>
2024-04-30 21:20:12 -0300

    Implementando método emprestar livro

---
+++ src/main/java/com/javaswing/dao/EmprestimoDao.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.javaswing.dao;
+
+
+import com.javaswing.modelo.Livro;
+import com.javaswing.modelo.Usuario;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ *
+ * @author felip
+ */
+public class EmprestimoDao  {
+
+  
+    public boolean emprestarLivro(Usuario usuario, Livro livro) {
+        
+        Conexao conexao = new Conexao();
+        Connection connection = conexao.conectar();
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate dataDevolucao = null;
+                
+        String query = "INSERT INTO emprestimo (usuario,livro,saida,devolucao,status) values (" +
+                "'" +   usuario.getCodigo()  + "'," +
+                "'" +   livro.getCodigo()   + "'," +
+                "'" +   dataAtual   + "'," +
+                "'" +   dataDevolucao + "'," +
+                usuario.getTipo()+ ")";
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            conexao.desconectar(connection);
+            return true;
+        }catch (SQLException ex) {
+            System.out.print(ex.initCause(ex));
+            return false;
+        } 
+    }
+
+    
+    public boolean devolverLivro(Usuario usuario, Livro livro) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+    public List<Livro> livroPorUsuario(Usuario usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
